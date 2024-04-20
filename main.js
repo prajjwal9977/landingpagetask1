af5a68879fd93bb1d0dd0882dd16ae6a99048130
prajjwal9977 <167640160+prajjwal9977@users.noreply.github.com>
2024-04-20 22:52:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var typed = new Typed(".text", {
+	strings: ["Frontend Developer" , "Data Analyst", "Web Developer"],
+	typeSpeed: 50,
+	backSpeed: 50,
+	backDelay: 1000,
+	loop: true
+});
+var typed = new Typed(".repeat", {
+	strings: ["Expertise is to create and website design, Frontend design, and many more...."],
+	typeSpeed: 20,
+	backSpeed: 10,
+	backDelay: 1000,
+	loop: true
+});
